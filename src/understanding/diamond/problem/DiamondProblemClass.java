58070620b9fbbf8e382e9c5c61b440e12017d20d
Lavesh Hemrajani <hemrajanilavesh@gmail.com>
2021-07-17 16:01:07 +0530

    fix typo

--- src/understanding/diamond/problem/DiamondProblemClass.java
+++ src/understanding/diamond/problem/DiamondProblemClass.java
@@ -6,7 +6,7 @@ package understanding.diamond.problem;
  * Since Java 8, an interface can have default methods.
  * If a class implements two interfaces which have same default method then this will result in diamond problem.
  * The Class implementing two interfaces with same default method will not compile.
- * Solution : Override the method and explicitly and call your super class method.
+ * Solution : Override the method explicitly and call your super class method.
  *
  */
 public class DiamondProblemClass implements MyInterfaceOne, MyInterfaceTwo {
